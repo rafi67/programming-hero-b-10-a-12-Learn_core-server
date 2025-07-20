b54f33bda5364ed19bb029494d3a7e410483feda
rafi67 <rafisiddique67@gmail.com>
2025-07-20 17:31:03 +0600

    collection added

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // db collection
+    const userCollection = client.db('eduDb').collection('user');
+    const userRoleCollection = client.db('eduDb').collection('userRole');
+    const feedbackCollection = client.db('eduDb').collection('feedback');
+    const teacherRequestCollection = client.db('eduDb').collection('teacherRequest');
+    const classCollection = client.db('eduDb').collection('class');
+    const enrollClassCollection = client.db('eduDb').collection('enrollClassCollection');
+    const submissionCollection = client.db('eduDb').collection('submission');
+    const assignmentCollection = client.db('eduDb').collection('assignment');
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
